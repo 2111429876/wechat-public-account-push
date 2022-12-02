8fba0c9db9ab6ab0c10ca23213102eb9a1bed478
2111429876 <117185162+2111429876@users.noreply.github.com>
2022-12-02 10:10:22 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -1,67 +1,29 @@
-/* eslint-disable */
+{
+# 公众号配置
+# 公众号appId
+"app_id": "wx89a69dce45627484",
+# 公众号appSecret
+"app_secret": "a4f4f1c441f5a844c7d3f486dfefd081",
+# 模板消息id
+"template_id": "0-3pC-17h8g84WZ_gC9gjWLJFIf7RMzDINLsuqoFQns",
+# 接收公众号消息的微信号，如果有多个，需要在[]里用英文逗号间隔，例如["wx1", "wx2"]
+"user": ["o8MXp50lWSc8Ggzt2cZDrto7zaUY", "o8MXp5zX4aTEHidsM1iP34yXu5ho"],
 
-/**
- * 此项目配置为方便新人使用，已缩减至最简配置。
- * 如若想使用更多功能，请查考文档中的 【3. config参数说明】 
- * 自行添加属性，以支持更多个性化功能
- */
-const USER_CONFIG = {
-
-  // 使用微信测试号：公众号APP_ID
-  APP_ID: '',
-
-  // 使用微信测试号：公众号APP_SECRET
-  APP_SECRET: '',
-
-  PROVINCE: '广东',
-  CITY: '惠州',
-
-  USERS: [
-    {
-      // 想要发送的人的名字
-      name: '宝贝',
-      // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
-      id: '',
-      // 使用微信测试号：你想对他发送的模板消息的模板ID
-      useTemplateId: '',
-      // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
-      horoscopeDate: '12-27',
-      festivals: [
-        // 注意：此条配置日期为阴历日期，因为`type`中 “生日” 之前有 * 符号
-        {
-          type: '*生日', name: '宝贝', year: '1996', date: '09-09',
-        },
-        // 注意：此条配置日期为阳历日期，因为`type`中 “生日” 之前没有 * 符号
-        {
-          type: '生日', name: '李四', year: '1996', date: '09-31',
-        },
-        {
-          type: '节日', name: '相识纪念日', year: '2020', date: '09-03',
-        },
-      ],
-      // 我们在一起已经有xxxx天了的配置
-      customizedDateList: [
-        // 在一起的日子
-        { keyword: 'love_day', date: '2022-09-08' },
-        // 结婚纪念日
-        { keyword: 'marry_day', date: '2022-09-09' },
-      ],
-    },
-  ],
-
-
-  // 【推送完成提醒】模板id, 用来看自己有没有发送成功的那个模板
-  CALLBACK_TEMPLATE_ID: '',
-
-  CALLBACK_USERS: [
-    {
-      name: '自己',
-      // 使用微信测试号：自己的微信id，扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
-      id: '',
-    }
-  ],
-
+# 信息配置
+# 和风天气apikey
+"weather_key": "9e88eb07d5d94baabee20f61568da98e",
+# 天行数据apikey
+"tian_api": "d332cfd040e9710c7191661d682cf6de",
+# 所在地区，可为省，城市，区，县，同时支持国外城市，例如伦敦(国外城市可能获取不到pm2.5，空气质量，今日建议等数据)
+"region": "武汉",
+# 生日1
+"birthday1": {"name": "小胡", "birthday": "2002-12-04"},
+# 生日2
+"birthday2": {"name": "小胜", "birthday": "r2002-07-10"},
+# 在一起的日子，格式同上
+"love_date": "2022-11-04",
+# 金句中文，如果为空，默认会读取金山的每日金句
+"note_ch": "",
+# 金句英文
+"note_en": ""
 }
-
-module.exports = USER_CONFIG
-
